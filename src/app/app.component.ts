6e7a64f2a28bb4b07d628e2de7d171d1d9bc0668
Hüseyin Soytürk <soyturk.huseyin61@gmail.com>
2018-07-19 08:52:27 +0300

    sabit en boyda box çizme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,26 +10,47 @@ import { of, Observable } from 'rxjs';
 export class AppComponent {
   title = 'app';
   customer = ''
-  cepheSayisi = 0;
+  cepheSayisi;
   cepheler = []
+  boxesConfig = []
+  ebatShow = true;
+  cepheSayisiShow = true
+
+  configStage: Observable<any> = of({
+    width: 1200,
+    height: 400
+  });
 
   onClickNext() {
     this.cepheler = []
     for (let i = 0; i < this.cepheSayisi; i++) {
-      this.cepheler.push(new Cephe('', '', ''))
+      this.cepheler.push(new Cephe(i, '', '', ''))
     }
+    // this.ebatShow = true
   }
 
-  configStage: Observable<any> = of({
-    width: 100,
-    height: 100
-  });
-  configCircle: Observable<any> = of({
-    x: 50,
-    y: 50,
-    radius: 50,
-    fill: 'red',
-    stroke: 'black',
-    strokeWidth: 4
-  });
+  onClickDraw() {
+    console.log("this.cepheSayisi", this.cepheSayisi);
+    console.log("this.cepheler", this.cepheler);
+
+    if (this.cepheSayisi == 1) {
+      for (let i = 0; i < this.cepheler[0].kanatSayisi; i++) {
+        let box = of({
+          x: 20 + i * 200,
+          y: 10,
+          width: 200,
+          height: 350,
+          fill: 'white',
+          stroke: 'black',
+          strokeWidth: 2,
+          name: 'kanat' + i.toString
+        });
+        this.boxesConfig.push(box)
+      }
+    }
+    // this.ebatShow = false
+    // this.cepheSayisiShow = false
+  }
+
+
 }
--- src/cephe.ts
+++ src/cephe.ts
@@ -2,6 +2,7 @@
 export class Cephe {
 
   constructor(
+    public id : number,
     public boy: string,
     public en: string,
     public kanatSayisi: string,
